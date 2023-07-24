961a155596dd790d42c4abaffad15b59e15b6007
Luswepo <Luswepo@DSS-RISKCOMP-ASS.DIGITALPAYGO.COM>
2023-07-24 10:09:10 +0200

    first commit

---
+++ app/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import spinner from "../../public/spinner.gif";
+import Image from "next/image";
+
+const Spinner = () => {
+  return (
+    <>
+      <Image className="w-[200px] m-auto block" alt="loading.." src={spinner} />
+    </>
+  );
+};
+
+export default Spinner;
